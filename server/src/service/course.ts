012f07851662e790f60cfe74093faab9c25e1ac0
Pontanami <pontusen@student.chalmers.se>
2024-01-30 15:50:08 +0100

    course router and service
    Co-Authored-By: Jrngrd <93658784+Jrngrd@users.noreply.github.com>
    Co-Authored-By: Alexander Persson <91194150+aalexpersson@users.noreply.github.com>

--- server/src/service/course.ts
+++ server/src/service/course.ts
@@ -1,15 +1,39 @@
 import {Course} from "../model/course";
 
 export class CourseService{
+    courses: Course[] = [];
 
     async createCourse(name: string, code: string, passrate: number): Promise<Course>{
-        let newCourse : Course = {
+        const newCourse : Course = {
             code: code, 
             name: name,
             failrate : 100 - passrate
         }
-
+        this.courses.push(newCourse)
         return { ... newCourse};
+    }
+
+    async getCourse(code: string): Promise<Course|undefined>{
+        const course = this.courses.find(course => course.code === code);
+        if(course){
+            return { ... course};
+        }
+        return undefined;
+        
+    }
+
+    async updateCourse(code: string, name: string, passrate: number): Promise<Course|undefined>{
+        const course = this.courses.find(course => course.code === code);
+        if(course){
+            course.name = name;
+            course.failrate = 100 - passrate;
+            return { ... course};
+        }
+        return undefined;
+    }
+
+    async getListOfCourses(): Promise<Course[]> {
+        return JSON.parse(JSON.stringify(this.courses));
     }
 
 }
--- server/src/service/session.ts
+++ server/src/service/session.ts
@@ -1,7 +1,8 @@
+import { Player } from "../model/player";
 import {Session} from "../model/session";
 
 export class SessionService{
-    //private currentPlayer : Player;
+    players : Player[] = [];
 
     async createSession(): Promise<Session>{
         let newSession : Session = {
@@ -10,6 +11,20 @@ export class SessionService{
         }
 
         return { ... newSession};
+    }
+
+    async addPlayerToSession(sessionId: number, player: Player): Promise<Session>{
+        this.players.push(player);
+        let newSession : Session = {
+            id : sessionId,
+            players : this.players
+        }
+
+        return { ... newSession};
+    }
+
+    async getSessionPlayers(sessionId: number): Promise<Player[]> {
+          return JSON.parse(JSON.stringify(this.players));
     }
 
 }
