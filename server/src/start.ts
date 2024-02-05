f862a49eacdd71e9b6990a8c0a99d64d6b99df22
Alexander Persson <alex.persson0408@gmail.com>
2024-02-05 15:48:34 +0100

    client_mockup

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { leaderboardRouter } from "./router/leaderboard";
 import { playerRouter } from "./router/player";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/leaderboard", leaderboardRouter);
 app.use("/player", playerRouter);
 
