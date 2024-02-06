8466e4c32b6add7a4cb16a4bebc3ceb6cfbb5828
Alexander Persson <alex.persson0408@gmail.com>
2024-02-06 11:38:04 +0100

    added homepage and leaderboard page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import logo from './Image/logo.png';
-import './App.css';
+import Home from './Home';
+import { Routes, Route } from 'react-router-dom';
+import Leaderboard from "./Leaderboard";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Higher or Lower</h1>
-      <img className="logo" src={logo}/>
-      <button className="question"></button>
-      <a href="leaderboard.html"><i className="bi bi-trophy">
-        </i></a>
-      <div className="container">
-        <a href="host.html" type="button">Host game</a>
-        <a href="playScreen.html" type="button">Single player</a>
-        <a href="join.html" type="button">Join game</a>
-      </div>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+        </Routes>
     </div>
   );
 }
