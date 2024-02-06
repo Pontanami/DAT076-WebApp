05842153f75263634f9d7077491b9f250a86d1eb
Alexander Persson <alex.persson0408@gmail.com>
2024-02-06 10:55:31 +0100

    started building mockup in react

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Leaderboard from './Leaderboard';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
