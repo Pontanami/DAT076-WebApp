ca943c32a170453d57eeb639b815c3640fdcbbda
aalexpersson <alex.persson0408@gmail.com>
2024-02-12 16:50:57 +0100

    "fixed port nr not showing"

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,5 +3,5 @@ import {app} from "./start";
 const PORT : number =  8080;
 
 app.listen(PORT, () => {
-    console.log("Listening on port $(PORT)");
+    console.log("Listening to port " + PORT);
 });
