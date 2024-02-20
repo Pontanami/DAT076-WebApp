14a61d89ee01c197eda545ac02ce03c8f05e2268
Pontanami <pontusen@student.chalmers.se>
2024-02-20 10:29:37 +0100

    init lab 4

---
+++ server/db/conn.ts
@@ -0,0 +1,5 @@
+import { createConnection } from "mongoose";
+
+
+
+export const conn = createConnection("mongodb+srv://pontusen:BbN1qpQz9QQuTQCo@chalmershigherlower.akkne3j.mongodb.net/?retryWrites=true&w=majority");
