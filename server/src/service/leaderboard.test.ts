4b8ad4c7dd71f54188e25363a68f590eaa1894f6
Jrngrd <erngard@student.chalmers.se>
2024-02-27 11:39:27 +0100

    Fix commit

--- server/src/service/leaderboard.test.ts
+++ server/src/service/leaderboard.test.ts
@@ -7,7 +7,7 @@ jest.mock("../db/conn.ts");
 test("If a player is added to the leaderboard they should be on the leadeboard", async () => {
     const leaderboardService = new LeaderboardService();
     let playerService = PlayerService.getInstance();
-    let player =  await playerService.createPlayer("test5")
+    let player =  await playerService.createPlayer(5,"test5")
     await leaderboardService.changeLeaderboard(player.id);
     const player_list = await leaderboardService.getPlayerEntries();
     expect(player_list.map((ply: Player) => ply.id)).toContain(player.id);
